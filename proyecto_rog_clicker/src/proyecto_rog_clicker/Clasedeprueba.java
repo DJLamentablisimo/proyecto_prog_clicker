af1ab7d0927671fb06bfd9dcc0069718a78e2167
Daniel Galeán <daniel.galean@opendeusto.es>
2021-10-27 21:30:07 +0200

    cosa

--- proyecto_rog_clicker/src/proyecto_rog_clicker/Clasedeprueba.java
+++ proyecto_rog_clicker/src/proyecto_rog_clicker/Clasedeprueba.java
@@ -3,6 +3,6 @@ package proyecto_rog_clicker;
 public class Clasedeprueba {
 public static void main(String[] args) {
 	System.out.println("dawdader");
-	System.out.println("cosa");
+	System.out.println("cosaa");
 }
 }
