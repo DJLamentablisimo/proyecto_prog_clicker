f981d97a95f085846779bd13e2ffdd8acba3a78f
Assaz <Assaz@DESKTOP-OS3L2NJ>
2021-10-27 20:53:59 +0200

    Confirmacion asier

--- proyecto_rog_clicker/src/proyecto_rog_clicker/prueba.java
+++ proyecto_rog_clicker/src/proyecto_rog_clicker/prueba.java
@@ -3,5 +3,6 @@ package proyecto_rog_clicker;
 public class prueba {
 	public static void main(String[] args) {
 		System.out.println("prueba234");
+		System.out.println("Asier lo ha logrado");
 	}
 }
