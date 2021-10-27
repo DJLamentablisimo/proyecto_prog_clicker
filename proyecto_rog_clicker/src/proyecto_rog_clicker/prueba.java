926d5f8036c63dfdea4a659542c4a3a56505f493
DJLamentablisimo <81747616+DJLamentablisimo@users.noreply.github.com>
2021-10-27 20:57:06 +0200

    Update prueba.java

--- proyecto_rog_clicker/src/proyecto_rog_clicker/prueba.java
+++ proyecto_rog_clicker/src/proyecto_rog_clicker/prueba.java
@@ -2,11 +2,9 @@ package proyecto_rog_clicker;
 
 public class prueba {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("prueba234");
 		System.out.println("Asier lo ha logrado");
-=======
 		System.out.println("Soy buenisimo");
->>>>>>> branch 'master' of https://github.com/DJLamentablisimo/proyecto_prog_clicker
+
 	}
 }
