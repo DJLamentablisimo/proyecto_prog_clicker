e14b2538514c1131833bf0275ed716ccd880f59f
Jorge Alonso <jorge.alonso2002@opendeusto.es>
2021-10-27 21:18:49 +0200

    Clase básica actualizada

--- proyecto_rog_clicker/src/proyecto_rog_clicker/ElementoJuego.java
+++ proyecto_rog_clicker/src/proyecto_rog_clicker/ElementoJuego.java
@@ -1,5 +1,18 @@
 package proyecto_rog_clicker;
 
 public class ElementoJuego {
-
+	private long precio;
+	private String nombre;
+	
+	public ElementoJuego(long precio, String nombre) {
+		this.precio = precio;
+		this.nombre = nombre;
+	}
+	
+	public void efecto() {
+		
+	}
+	
+	
+	
 }
