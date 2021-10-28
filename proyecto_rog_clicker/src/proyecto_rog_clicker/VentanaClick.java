dbf02e3e479e7acd0619cdd7f799b36b44d5e974
Jorge Alonso <jorge.alonso2002@opendeusto.es>
2021-10-28 21:54:56 +0200

    Ventana

---
+++ proyecto_rog_clicker/src/proyecto_rog_clicker/VentanaClick.java
@@ -0,0 +1,66 @@
+package proyecto_rog_clicker;
+
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class VentanaClick extends JFrame {
+
+	private static final long serialVersionUID = -4776141433437148579L;
+	private JButton cerdito;
+	private JLabel blanco;
+	private JLabel puntuacion;
+	private JPanel superior;
+	int dinero_click = 0;
+	int mult_click = 1;
+	
+	public VentanaClick() {
+		
+		JPanel superior = new JPanel();
+		JButton cerdito = new JButton();
+		JLabel blanco = new JLabel();
+		JLabel puntuacion = new JLabel("Ca$h Money Baby: "+String.valueOf(dinero_click));
+		puntuacion.setFont(new Font("Castellar", Font.BOLD, 20));
+		try {
+		    Image img = ImageIO.read(getClass().getResource("cerdito150.png"));
+		    cerdito.setIcon(new ImageIcon(img));
+		  } catch (Exception ex) {
+		    System.out.println(ex);
+		  }
+		cerdito.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dinero_click=dinero_click+1;
+				System.out.println(dinero_click);
+				puntuacion.setText("Ca$h Money Baby: "+String.valueOf(dinero_click));
+			}
+		});
+		getContentPane().setLayout(new BorderLayout());
+		
+		this.setSize(1680, 1020);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		this.setAlwaysOnTop(true);
+		this.setTitle("Business Go Boom");
+		this.setLocation(150, 10);
+		this.setMinimumSize(this.getSize());
+		getContentPane().add(superior, BorderLayout.NORTH);
+		superior.add(blanco, BorderLayout.EAST);
+		superior.add(cerdito, BorderLayout.CENTER);
+		superior.add(puntuacion, BorderLayout.WEST);
+	}
+	
+	public static void main(String[] args) {
+		VentanaClick v = new VentanaClick();
+	}
+}
