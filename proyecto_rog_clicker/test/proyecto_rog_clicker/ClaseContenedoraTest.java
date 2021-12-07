55849c78679ec27ed6e361644d13fc1724f2b8ca
Jorge Alonso <jorge.alonso2002@opendeusto.es>
2021-12-07 13:33:35 +0100

    Solución de problemas de build path

---
+++ proyecto_rog_clicker/test/proyecto_rog_clicker/ClaseContenedoraTest.java
@@ -0,0 +1,14 @@
+package proyecto_rog_clicker;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClaseContenedoraTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
