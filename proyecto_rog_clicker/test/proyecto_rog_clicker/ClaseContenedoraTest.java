351661bf6662266e96bba62a9aae946985550ac2
DavidBarrenechea <david.barrenechea@opendeusto.es>
2021-12-07 17:48:41 +0100

    David---Junit

--- proyecto_rog_clicker/test/proyecto_rog_clicker/ClaseContenedoraTest.java
+++ proyecto_rog_clicker/test/proyecto_rog_clicker/ClaseContenedoraTest.java
@@ -2,18 +2,32 @@ package proyecto_rog_clicker;
 
 import static org.junit.Assert.*;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+
 import org.junit.Test;
+
+import clases.Edificios;
+import clases.Usuario;
+import ventanas.ClaseContenedora;
 
 public class ClaseContenedoraTest {
 
+	private ClaseContenedora cc = new ClaseContenedora();
 	
-	
-	
+
 	@Test
 	public void testSacarUsuarios() {
-	
+		ArrayList<Usuario> lista = cc.sacarUsuarios();
+		//lista.add(new Usuario("Pepito","grillo",100,100,100));
+		assertEquals(3, lista.size());
 	}
-	
+
 	@Test
 	public void testActualizarPartida() {
 		
@@ -21,17 +35,26 @@ public class ClaseContenedoraTest {
 	
 	@Test
 	public void testSacarEdificios() {
-		
+		ArrayList<Edificios> lista = cc.sacarEdificios();
+		lista.add(new Edificios("Barco",100,100,100,""));
+		assertEquals(18,lista.size());
 	}
-	
+	/**
+
 	@Test
 	public void testGuardarDBUsuario() {
-		
+		cc.guardarDBUsuario(100, 100, 100, "Pepito", "grillo", 100);	
+		ArrayList<Usuario> lista = cc.sacarUsuarios();
+		assertTrue((lista.get(lista.size()-1).getnUsuario().equals("pepito")));	
 	}
-	
+/**
+ * 
+ */
 	@Test
 	public void testBorrarDBUsuario() {
-		
+		cc.borrarDBUsuario("pepito");
+		ArrayList<Usuario> lista = cc.sacarUsuarios();
+		assertEquals(2,lista.size());
 	}
 	
 	@Test
