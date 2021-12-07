0624190141d26dccc0537d50cb5fdd6faf8ac9c2
DavidBarrenechea <david.barrenechea@opendeusto.es>
2021-12-07 15:09:42 +0100

    David---Planteamiento JUnit

--- proyecto_rog_clicker/test/proyecto_rog_clicker/ClaseContenedoraTest.java
+++ proyecto_rog_clicker/test/proyecto_rog_clicker/ClaseContenedoraTest.java
@@ -6,9 +6,54 @@ import org.junit.Test;
 
 public class ClaseContenedoraTest {
 
+	
+	
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSacarUsuarios() {
+	
 	}
+	
+	@Test
+	public void testActualizarPartida() {
+		
+	}
+	
+	@Test
+	public void testSacarEdificios() {
+		
+	}
+	
+	@Test
+	public void testGuardarDBUsuario() {
+		
+	}
+	
+	@Test
+	public void testBorrarDBUsuario() {
+		
+	}
+	
+	@Test
+	public void testCambiarContraseñaBD() {
+		
+	}
+	
+	@Test
+	public void testGuardarDBEdificio() {
+		
+	}
+	
+	@Test
+	public void testBorrrarDBEdificio() {
+		
+	}
+	
+	@Test
+	public void testBorrarMejoras() {
+		
+	}
+	
+	
 
 }
