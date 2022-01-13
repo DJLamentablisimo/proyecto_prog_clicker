ec079f8db2baf8b6ee4bca1ca9e713b76fca5fc3
DavidBarrenechea <david.barrenechea@opendeusto.es>
2022-01-13 17:09:32 +0100

    Junit

--- vamoa aver/desktop/test/proyecto_rog_clicker/ClaseContenedoraTest.java
+++ vamoa aver/desktop/test/proyecto_rog_clicker/ClaseContenedoraTest.java
@@ -1,5 +1,3 @@
-package proyecto_rog_clicker;
-
 import static org.junit.Assert.*;
 
 import java.sql.Connection;
@@ -26,16 +24,16 @@ public class ClaseContenedoraTest {
 
 	@Test
 	public void testSacarUsuarios() {
-		ArrayList<Usuario> lista = cc.sacarUsuarios("Usuario.db");
-		assertEquals(2, lista.size());
+		ArrayList<Usuario> lista = cc.sacarUsuarios("UsuarioTest.db");
+		assertEquals(6, lista.size());
 	}
 
 	//Comprueba si los datos de juego de un jugador se han actualizado correctamente en la base de datos.
 	
 	@Test
 	public void testActualizarPartida() {
-		ArrayList<Usuario>lista = cc.sacarUsuarios("Usuario.db");
-		cc.actualizarPartida("Usuario.db","admin", 0, 0, 0);
+		ArrayList<Usuario>lista = cc.sacarUsuarios("UsuarioTest.db");
+		cc.actualizarPartida("UsuarioTest.db","admin", 0, 0, 0);
 		for (Usuario u: lista) {
 			if(u.getnUsuario().equals("admin")) {
 				assertEquals(0,u.getDinero_click_personal());
@@ -48,31 +46,31 @@ public class ClaseContenedoraTest {
 	//Comprueba que la cantidad de edificios guardados en la base de datos es correcta
 	@Test
 	public void testSacarEdificios() {
-		ArrayList<Edificios> lista = cc.sacarEdificios("Usuario.db");
+		ArrayList<Edificios> lista = cc.sacarEdificios("UsuarioTest.db");
 		assertEquals(17,lista.size());
 	}
 	
 	//Comprueba que se guarda de manera correcta una nuevo usuario en la base de datos
 	@Test
 	public void testGuardarDBUsuario() {
-		ArrayList<Usuario> lista = cc.sacarUsuarios("Usuario.db");
-		cc.guardarDBUsuario("Usuario.db",100, 100, 100, "pepito", "grillo");
+		ArrayList<Usuario> lista = cc.sacarUsuarios("UsuarioTest.db");
+		cc.guardarDBUsuario("UsuarioTest.db",100, 100, 100, "pepito", "grillo");
 		assertTrue((lista.get(lista.size()-1).getnUsuario().equals("pepito")));	
 	}
 	
 	//Comprueba que se borra correctamente un usuario de la base de datos
 	@Test
 	public void testBorrarDBUsuario() {
-		ArrayList<Usuario>lista = cc.sacarUsuarios("Usuario.db");
-		cc.borrarDBUsuario("Usuario.db","pepito");
-		assertEquals(2,lista.size());
+		ArrayList<Usuario>lista = cc.sacarUsuarios("UsuarioTest.db");
+		cc.borrarDBUsuario("UsuarioTest.db","pepito");
+		assertEquals(6,lista.size());
 	}
 	
 	//Comprueba que la contraseña se ha cambiado correctamente en la base de datos
 	@Test
 	public void testCambiarContraseñaBD() {
-		ArrayList<Usuario>lista = cc.sacarUsuarios("Usuario.db");
-		cc.cambiarContraseñaBD("Usuario.db","admin", "123", "012");
+		ArrayList<Usuario>lista = cc.sacarUsuarios("UsuarioTest.db");
+		cc.cambiarContraseñaBD("UsuarioTest.db","admin", "123", "012");
 	for (Usuario u: lista) {
 		if(u.getnUsuario().equals("admin")) {
 			assertEquals("012",u.getContraseña());		
@@ -83,19 +81,20 @@ public class ClaseContenedoraTest {
 	//Comprueba que se guarda de manera correcta un nuevo edificio en la base de datos
 	@Test
 	public void testGuardarDBEdificio() {
-	ArrayList<Edificios> lista = cc.sacarEdificios("Usuario.db");
-	cc.guardarDBEdificio("Usuario.db","prueba", 100, 100, 100, "");
-	assertTrue((lista.get(lista.size()-1).getNombre().equals("pepito")));	
+	ArrayList<Edificios> lista = cc.sacarEdificios("UsuarioTest.db");
+	cc.guardarDBEdificio("UsuarioTest.db","prueba", 100, 100, 100, "");
+	assertTrue((lista.get(lista.size()-1).getNombre().equals("prueba")));	
 	}
 	
 	//Comprueba que se borra correctamente un edificio de la base de datos
 	@Test
 	public void testBorrrarDBEdificio() {
-		ArrayList<Edificios> lista = cc.sacarEdificios("Usuario.db");
-		cc.borrarDBEdificio("Usuario.db","prueba");
+		ArrayList<Edificios> lista = cc.sacarEdificios("UsuarioTest.db");
+		cc.borrarDBEdificio("UsuarioTest.db","prueba");
 		assertEquals(17,lista.size());
-		
+	
 	}
+	/**	
 	@Test
 	public void testGuardarMejoras() {
 		
@@ -125,6 +124,7 @@ public class ClaseContenedoraTest {
 	public void testActualizarPersonalEdifi2() {
 		
 	}
-	
+	**/
+		
 
 }
